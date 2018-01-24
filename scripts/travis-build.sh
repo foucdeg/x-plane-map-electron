fe35cee2fc8f9d325e33db2c6f04c176a688bc9a
Foucauld Degeorges <foucdeg@gmail.com>
2018-01-24 11:09:51 +0100

    Fix Mac build ?

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,6 +4,7 @@ git clone https://github.com/creationix/nvm.git /tmp/.nvm
 source /tmp/.nvm/nvm.sh
 nvm install "$NODE_VERSION"
 nvm use --delete-prefix "$NODE_VERSION"
+npm i -g yarn
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
@@ -12,5 +13,5 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 fi
 
 yarn
-yarn run compile
+NODE_ENV=production yarn run compile
 
